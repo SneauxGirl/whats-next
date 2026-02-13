558f7f221cbe1ee683be3029b7bd2d68d032d178
SneauxGirl <heatherh@liyudev.com>
2026-02-13 03:13:13 -0800

    Products: commented out fakestore code for deployment

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,23 +1,32 @@
-export const generateStaticParams = async () => {
-    const products = await fetch ('https://fakestoreapi.com/products').then(response => response.json());
-    return products.map(product => ({ id: product.id.toString() }))
-}
-
-export const ProductsPage = async () => {
-    const products = await fetch ('https://fakestoreapi.com/products').then(response => response.json());
-
+const ProductsPage = () => { //Swap with below to work in local, swap back before deployment
     return (
-        <div>
-            <h2>Products</h2>
-            <ul>
-                {products.map(product => (
-                    <li key={product.id}>
-                        <h3>{product.title}</h3>
-                        <p>${product.price}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <>
+            <h1>Products</h1>
+        </>
     )
 }
+
+
+// export const generateStaticParams = async () => {
+//     const products = await fetch ('https://fakestoreapi.com/products').then(response => response.json());
+//     return products.map(product => ({ id: product.id.toString() }))
+// }
+
+// export const ProductsPage = async () => {
+//     const products = await fetch ('https://fakestoreapi.com/products').then(response => response.json());
+
+//     return (
+//         <div>
+//             <h2>Products</h2>
+//             <ul>
+//                 {products.map(product => (
+//                     <li key={product.id}>
+//                         <h3>{product.title}</h3>
+//                         <p>${product.price}</p>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
 export default ProductsPage;
